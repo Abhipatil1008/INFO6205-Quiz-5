44f1c16e109d14e075968fc6d2732d22662e532c
Abhipatil1008 <patil.abhishek@northeastern.edu>
2025-03-20 13:08:06 -0400

    Quiz5 completed

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -3,6 +3,19 @@ class SelectionSort {
         int n = arr.length;
 
        // STUDENT TODO
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+
+        }
+
     }
 
     public static void main(String[] args) {
